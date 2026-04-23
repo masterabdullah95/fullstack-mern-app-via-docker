6efc6487f79598b514c8cc6bfa38f63aaf6e3c28
Abdullah Shoaib <masterabdullah95@gmail.com>
2026-04-23 15:53:25 +0500

    added backend url in cors

--- express-container/index.js
+++ express-container/index.js
@@ -5,7 +5,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://fullstack-backend-docker-production.up.railway.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"], // allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // allow cookies / auth headers
