947d652a601b90244862802e99c2195d739086f3
Abdullah Shoaib <masterabdullah95@gmail.com>
2026-04-23 14:42:09 +0500

    changes port and added .env support

--- express-container/index.js
+++ express-container/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 const app = express();
 
 app.use(
@@ -15,7 +16,7 @@ app.get("/", (req, res) => {
   res.json({ msg: "hello from express container" });
 });
 
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () =>
   console.log(`Server started at http://localhost:${port}`),
