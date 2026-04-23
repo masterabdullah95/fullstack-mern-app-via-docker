b3c28d5e329f99a090be31cf7fe201f17f62e709
Abdullah Shoaib <masterabdullah95@gmail.com>
2026-04-23 17:05:58 +0500

    Added mongodb as a new container

--- express-container/index.js
+++ express-container/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import { mongoose } from "mongoose";
 import cors from "cors";
 import "dotenv/config";
 const app = express();
+app.use(express.json());
+
+// Add this middleware (already built into Express, no install needed)
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
@@ -15,6 +20,32 @@ app.use(
     credentials: true, // allow cookies / auth headers
   }),
 );
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB error:", err));
+
+// Simple Schema
+const ItemSchema = new mongoose.Schema({
+  name: String,
+  createdAt: { type: Date, default: Date.now },
+});
+const Item = mongoose.model("Item", ItemSchema);
+
+// GET - fetch all items
+app.get("/items", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+// POST - add an item
+app.post("/items", async (req, res) => {
+  const item = new Item({ name: req.body.name });
+  await item.save();
+  res.json(item);
+});
 
 app.get("/", (req, res) => {
   res.json({ msg: "hello from express container" });
--- react-container/src/App.jsx
+++ react-container/src/App.jsx
@@ -2,20 +2,26 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    console.log(import.meta.env.VITE_API_URL);
-    fetch(`${import.meta.env.VITE_API_URL}`)
+    console.log(import.meta.env.VITE_API_URL + "/items");
+    fetch(`${import.meta.env.VITE_API_URL + "/items"}`)
       .then((res) => res.json())
-      .then((data) => setMessage(data.msg));
+      .then((data) => setItems(data));
   }, []);
 
   return (
     <>
       <section id="center">
         <h2>This is test</h2>
-        <h2>{message}</h2>
+        <ul>
+          {items.length > 0 ? (
+            items.map((item, key) => <li key={key}>{item.name}</li>)
+          ) : (
+            <li>No data found</li>
+          )}
+        </ul>
       </section>
     </>
   );
